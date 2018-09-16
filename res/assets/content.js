8347be1dce8e9ee5db47243ea162f7e866418c07
chongyi <xpz3847878@163.com>
2018-09-17 02:42:41 +0800

    固定目录

--- res/assets/content.js
+++ res/assets/content.js
@@ -5,9 +5,30 @@ document.addEventListener("DOMContentLoaded", () => {
         articles: document.querySelectorAll('.content-body h2, .content-body h3, .content-body h4, .content-body h5, .content-body h6')
     })
 
-    let hold = document.getElementsByClassName('hold')[0]
+    let holds = document.querySelectorAll('.hold')
+    let handlers = []
+    holds.forEach((el) => {
+        let p = el.offsetParent
+        let top = el.offsetTop
+        while (p !== document.body) {
+            top += p.offsetTop
+            p = p.offsetParent
+            console.log(top)
+        }
+
+        handlers.push({w: el.offsetWidth, top, el})
+    })
     document.getElementById('toc').appendChild(contents.list())
     document.addEventListener('scroll', (e) => {
-
+        let top = document.documentElement.scrollTop
+        handlers.forEach((t) => {
+            if (top >= t.top && !t.el.classList.contains('pin')) {
+                t.el.classList.add('pin')
+                t.el.style.width = `${t.w}px`
+            } else if (top < t.top && t.el.classList.contains('pin')) {
+                t.el.classList.remove('pin')
+                t.el.style.width = ''
+            }
+        })
     })
 })

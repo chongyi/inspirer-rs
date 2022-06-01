cbd255ba04fdf480fc7601d98ff805b4676276cf
Chongyi <xpz3847878@163.com>
2022-06-01 14:54:05 +0800

    Support daemonize

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
-mod server;
+use std::process::ExitCode;
+
 mod controller;
-mod route;
-mod response;
 mod error;
 mod request;
+mod response;
+mod route;
+mod server;
 
 fn main() {
     dotenv::dotenv().expect("Initialize dotenv error.");
+
+    #[cfg(target_family = "unix")]
+    {
+        let daemonize = std::env::var("DAEMONIZE")
+            .map(|s| s == "true" || s == "1")
+            .unwrap_or(false);
+
+        if daemonize {
+            let daemon = Daemon::new()
+                .start();
+
+            match daemon {
+                Ok(_) => (),
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                    std::process::exit(ExitCode::FAILURE);
+                }
+            }
+        }
     
+    }
+
     server::run().unwrap();
+
+    std::process::exit(ExitCode::SUCCESS);
 }
